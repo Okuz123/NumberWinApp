6d943fc64d799ded5e2d36a8285cd8524d8fcf08
OVAIS KHANDAY <96727163+Okuz123@users.noreply.github.com>
2025-06-05 19:01:06 +0530

    Create MainActivity.java

---
+++ NumberWinApp/app/src/main/java/com/example/numberwin/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.numberwin;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // Enable JavaScript
+        webView.loadUrl("file:///android_asset/index.html"); // Load HTML from assets
+    }
+}
